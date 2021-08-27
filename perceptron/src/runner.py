c46a0be8e8597ea87aeed9dd161ac87e289f9d97
arthurzatta <arthur.zatta@gmail.com>
2021-08-27 00:34:38 -0300

    Alterações

--- perceptron/src/runner.py
+++ perceptron/src/runner.py
@@ -4,20 +4,22 @@ from perceptron import Perceptron
 
 
 class Runner:
-    def __init__(self, dataset: ndarray, ages: int, min_value: float, max_value: float, perceptron: Perceptron):
+    def __init__(self, dataset: ndarray, ages: int, min_value: float, max_value: float, n_out:int, perceptron: Perceptron):
         self.dataset = dataset
         self.min = min_value
         self.max = max_value
         self.ages = ages
         self.__weights = []
         self.perceptron = perceptron
+        self.n_out = n_out
         self.errors = []
 
     def executer(self):
 
         length = self.dataset[0].size
 
-        self.__weights = np.random.uniform(low=self.min, high=self.max, size=(length))
+        #Gera a matriz de pesos
+        self.__weights = np.random.uniform(low=self.min, high=self.max, size=(self.n_out, length))
         
         for age in range(self.ages):
 
@@ -26,34 +28,40 @@ class Runner:
             for j in range(len(self.dataset)):
                 sample_array = np.append([1], self.dataset[j])
                 error = self.runner_sample(sample_array)
-                errors = np.append(errors, [error])
+                errors = np.append(errors, error)
 
             total_error = self.age_total_error(errors)
-            self.printer(age=age, error=total_error)   
+            # self.printer(age=age, error=total_error)   
 
-    def runner_sample(self, dataset_row: ndarray):
+    def runner_sample(self, dataset_row: ndarray) -> ndarray:
 
-        idx = dataset_row.size-1
-        expected_result = dataset_row[idx]
+        idx = dataset_row.size-self.n_out
 
-        # Somatorio
-        summation = self.perceptron.summation(dataset_row[:idx], self.__weights)
+        #Array de resultados
+        expected_results = dataset_row[idx:]
+        errors = []
 
-        # Sigmoidal
-        sigm = self.perceptron.sigmoidal(summation)
+        for i in range(expected_results.size):
+            print(self.__weights[i])
+            #Somatorio
+            summation = self.perceptron.summation(dataset_row[:idx], self.__weights[i])
 
-        # Error
-        error = self.perceptron.error(expected_result, sigm)
+            # Sigmoidal
+            sigm = self.perceptron.sigmoidal(summation)
 
-        # Novos pesos
-        for i in range(self.__weights.size):
-            self.__weights[i] = self.perceptron.new_weigth(
-                expected_value=expected_result,
-                sigm=sigm,
-                xi=dataset_row[i],
-                weigth=self.__weights[i]
-            )
+            # Error
+            errors.append(self.perceptron.error(expected_results[i], sigm))
 
+            # Novos pesos
+            for j in range(self.__weights[i].size):
+                self.__weights[i][j] = self.perceptron.new_weigth(
+                    expected_value=expected_results[i],
+                    sigm=sigm,
+                    xi=dataset_row[i],
+                    weigth=self.__weights[i][j]
+                )
+        errors = np.asarray(errors)
+        error = self.age_total_error(errors)
         return error
 
     def age_total_error(self, error: ndarray):
