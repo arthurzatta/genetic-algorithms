6ccc8fc01744e44b8d1e3d04a654d748e7469c83
arthur.zatta <arthur.zatta@gmail.com>
2021-11-21 16:44:59 -0300

    Index da função delta_h corrigido

--- mlp/src/main.py
+++ mlp/src/main.py
@@ -1,16 +1,16 @@
 from model.dataset import and_table, or_table, xor_table, robot_table
 from services.mlp import MLP
 
-AGES = 100000
+AGES = 10000
 MIN = -0.3
 MAX = 0.3
 
 print("TABELA AND")
-and_resolution = MLP(qtd_in=2, qtd_out=1, qtd_h=2, dataset=and_table, ages=AGES, min_value=MIN, max_value=MAX).executar()
+and_resolution = MLP(qtd_in=2, qtd_out=1, qtd_h=2, dataset=and_table, ages=AGES, min_value=MIN, max_value=MAX, ni=0.3).executar()
 print("TABELA OR")
-or_resolution = MLP(qtd_in=2, qtd_out=1, qtd_h=2, dataset=or_table, ages=AGES, min_value=MIN, max_value=MAX).executar()
+or_resolution = MLP(qtd_in=2, qtd_out=1, qtd_h=2, dataset=or_table, ages=AGES, min_value=MIN, max_value=MAX, ni=0.3).executar()
 print("TABELA XOR")
-xor_resolution = MLP(qtd_in=2, qtd_out=1, qtd_h=2, dataset=xor_table, ages=AGES, min_value=MIN, max_value=MAX).executar()
+xor_resolution = MLP(qtd_in=2, qtd_out=1, qtd_h=2, dataset=xor_table, ages=AGES, min_value=MIN, max_value=MAX, ni=0.3).executar()
 print("TABELA ROBO")
-robot_resolution = MLP(qtd_in=3, qtd_out=2, qtd_h=3, dataset=robot_table, ages=AGES, min_value=MIN, max_value=MAX).executar()
+robot_resolution = MLP(qtd_in=3, qtd_out=2, qtd_h=3, dataset=robot_table, ages=AGES, min_value=MIN, max_value=MAX, ni=0.3).executar()
 
--- mlp/src/services/mlp.py
+++ mlp/src/services/mlp.py
@@ -6,7 +6,7 @@ from interfaces.rna import RNA
 
 class MLP(RNA):
 
-    def __init__(self, qtd_in: int, qtd_h: int, qtd_out: int, dataset: ndarray, ages: int, min_value: float, max_value: float):
+    def __init__(self, qtd_in: int, qtd_h: int, qtd_out: int, dataset: ndarray, ages: int, min_value: float, max_value: float, ni: float):
         self.qtd_in = qtd_in
         self.qtd_out = qtd_out
         self.qtd_h = qtd_h
@@ -16,6 +16,7 @@ class MLP(RNA):
         self.max = max_value
         self.weigth_hiden = self.weigth_matrix(self.qtd_in+1, self.qtd_h)
         self.weigth_theta = self.weigth_matrix(self.qtd_h+1, self.qtd_out)
+        self.ni = ni
 
     def aproximation_error(self, expected_output: ndarray, sigmoid: ndarray):
         result = 0
@@ -23,8 +24,25 @@ class MLP(RNA):
             result += abs(expected_output[i] - sigmoid[i])
         return result
 
+    def classification_error(self, expected_output: ndarray, output: ndarray):
+        threshold_array = np.zeros(output.size)
+
+        for sample in range(output.size):
+            threshold_array[sample] = self.threshold(sample)
+
+        aprox_error = self.aproximation_error(expected_output, threshold_array)
+
+        if aprox_error > 0: 
+            return 1
+        return 0
+        
     def sigmoid(self, value: float):
         return 1/(1 + exp(-value))
+    
+    def threshold(self, theta: float):
+        if theta >= 0.5: 
+            return 1
+        return 0
 
     def delta_theta(self, theta_array: ndarray, expected_output: ndarray) -> ndarray:
         delta_theta = np.copy(theta_array)
@@ -35,11 +53,13 @@ class MLP(RNA):
 
     def delta_h(self, hiden_array: ndarray, w_theta: ndarray, delta_theta: ndarray):
         delta_h = np.copy(hiden_array)
-        for h in range(hiden_array.size):
+        # print(delta_h)
+        # input()
+        for h in range(len(delta_h)):
             summation = 0
             for j in range(delta_theta.size):
                 summation += delta_theta[j] * w_theta[h][j]
-            delta_h[j] = hiden_array[h] * (1-hiden_array[h]) * summation
+            delta_h[h] = hiden_array[h] * (1-hiden_array[h]) * summation
         return delta_h
 
     def weigth_matrix(self, rows: int, columns: int):
@@ -53,8 +73,8 @@ class MLP(RNA):
                 self.weigth_theta[h][j] += ni * delta_theta[j] * hiden_array[h]
 
     def hiden_weigth_adjustment(self, delta_hiden: ndarray, ni: float, x_in: ndarray):
-        for i in range(len(delta_hiden)):
-            for h in range(delta_hiden.size-1):
+        for i in range(len(self.weigth_hiden)):
+            for h in range(len(self.weigth_hiden[0])):
                 self.weigth_hiden[i][h] += ni * delta_hiden[h] * x_in[i]
 
     def age_total_error(self, error: ndarray):
@@ -86,29 +106,36 @@ class MLP(RNA):
                 sum_result += H[h] * self.weigth_theta[h][j]
             output[j] = self.sigmoid(sum_result)
 
-        error = self.aproximation_error(y, output)
-
         delta_theta = self.delta_theta(output, y)
-        delta_h = self.delta_h(H, self.weigth_hiden, delta_theta)
+        delta_h = self.delta_h(H, self.weigth_theta, delta_theta)
+        
         self.theta_weigth_adjustment(
-            ni=0.3, delta_theta=delta_theta, hiden_array=H)
-        self.hiden_weigth_adjustment(delta_hiden=delta_h, ni=0.3, x_in=x)
+            ni=self.ni, delta_theta=delta_theta, hiden_array=H)
+        self.hiden_weigth_adjustment(delta_hiden=delta_h, ni=self.ni, x_in=x)
 
-        return error
+        return output
 
     def executar(self):
 
+        # errors = np.zeros(len(self.dataset))
+        errors = {
+            "aproximation": np.zeros(len(self.dataset)),
+            "classification": np.zeros(len(self.dataset))
+        }
+
         for age in range(self.ages):
-
-            errors = np.asarray([])
-
             for j in range(len(self.dataset)):
 
                 x_in = self.dataset[j][:self.qtd_in]
                 y = self.dataset[j][self.qtd_in:]
 
-                error = self.treinar(x_in, y)
-                errors = np.append(errors, [error])
+                output = self.treinar(x_in, y)
+                aprx_error = self.aproximation_error(y, output)
+                class_error = self.classification_error(y, output)
 
-            total_error = self.age_total_error(errors)
-            self.printer(age=age, error=total_error)
+                errors["aproximation"][j] = aprx_error
+                errors["classification"][j] = class_error
+            
+            total_error_aprox = self.age_total_error(errors["aproximation"])
+            total_error_class = self.age_total_error(errors["classification"])
+            self.printer(age=age, error=total_error_aprox)
